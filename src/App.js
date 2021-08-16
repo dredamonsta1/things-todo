59d263f64cecfdde21022c283097c28788a4e953
andre <andre_w3@yahoo.com>
2021-08-16 02:54:30 -0400

    added nanoid for individual id's

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { nanoid } from "nanoid";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
@@ -7,7 +8,7 @@ function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
 
   function addTask(name){
-    const newTask = { id: "id", name: name, completed: false };
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
     setTasks([...tasks, newTask]);
   }
 
