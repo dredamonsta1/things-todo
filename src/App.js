d749c331e969f19ec53a9b498fa456c7e8d33d55
andre <andre_w3@yahoo.com>
2021-08-17 21:30:09 -0400

    delete funcionality working

--- src/App.js
+++ src/App.js
@@ -31,6 +31,18 @@ function App(props) {
     setTasks(remainingTasks)
   }
 
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map(task => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        return {...task, name: newName}
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+
+  }
+
   const taskList = tasks.map(task => (
     <Todo
       id={task.id}
@@ -39,10 +51,10 @@ function App(props) {
       key={task.id}
       toggleTaskCompleted={toggleTaskCompleted}
       deleteTask={deleteTask}
+      editTask={editTask}
 
     />
-  )
-);
+  ));
 
 const tasksNoun = taskList.length !==1 ? 'tasks' : 'task';
 const headingText = `${taskList.length} ${tasksNoun} remainig`;
